c115e46a14300fe3ec0076237c05fa3a96154f23
GueTorres <guevara.torres619@outlook.com>
2018-12-10 14:13:31 -0500

    Update PATGUI.java

--- Personality Assessment Test/src/PAT/PATGUI.java
+++ Personality Assessment Test/src/PAT/PATGUI.java
@@ -18,7 +18,7 @@ public class PATGUI extends JFrame implements ActionListener {
 	private JButton jbCreateFile, jbDisplayFile, jbDisplay1stLine, jbDeleteFile,
 	jbWriteToFile, jbCopyFile;
 	private JTextArea jTextArea;
-	private static final int GUI_WIDTH = 1000;
+	private static final int GUI_WIDTH = 500;
 	private static final int GUI_HEIGHT = 500;
 	private static final int NUM_COLS_TEXT_AREA = GUI_WIDTH/15;
 	private static final int NUM_ROWS_TEXT_AREA = GUI_HEIGHT/200;
@@ -27,7 +27,7 @@ public class PATGUI extends JFrame implements ActionListener {
 	
 	public PATGUI(){
 		JPanel mainJP = new JPanel();
-		mainJP.setLayout(new GridLayout(3, 3));
+		mainJP.setLayout(new FlowLayout(CENTER, 0, 50));
 		mainJP.setBackground(Color.MAGENTA);
 		jbCreateFile = new JButton(ACTION_COMMAND_CREATE_FILE);//can do the same for others to avoid typo problems
 		//more here		
@@ -47,14 +47,14 @@ public class PATGUI extends JFrame implements ActionListener {
 		
 		jTextArea = new JTextArea(NUM_ROWS_TEXT_AREA,NUM_COLS_TEXT_AREA);
 				
-		mainJP.add(jbCreateFile, 1, 0);
-		//mainJP.add(jlSession, 0, 3);		
-		mainJP.add(jlAnswer, 0, 2);		
-		mainJP.add(jTextArea, 1, 2);		
-		mainJP.add(jbNextQuestion, 1, 0);		
-		mainJP.add(jbDisplayQA, 1, 1);
-		mainJP.add(jbDisplayLongestWord, 2, 1);
-		mainJP.add(jbDisplayLWordAB, 0, 1);
+		mainJP.add(jbCreateFile;
+		mainJP.add(jlSession);		
+		//mainJP.add(jlAnswer);		
+		mainJP.add(jTextArea);		
+		mainJP.add(jbNextQuestion);		
+		mainJP.add(jbDisplayQA);
+		mainJP.add(jbDisplayLongestWord);
+		mainJP.add(jbDisplayLWordAB);
 		
 		/**mainJP.add(jbCreateFile);
 		mainJP.add(jbDisplayFile);
@@ -94,9 +94,9 @@ public class PATGUI extends JFrame implements ActionListener {
 			jlSession.setVisible(true);
 			jlAnswer.setVisible(true);
 			jbNextQuestion.setVisible(true);
-			jbDisplayQA.setVisible(true);
-			jbDisplayLongestWord.setVisible(true);
-			jbDisplayLWordAB.setVisible(true);
+			//jbDisplayQA.setVisible(true);
+			//jbDisplayLongestWord.setVisible(true);
+			//jbDisplayLWordAB.setVisible(true);
 			
 			break;
 			
@@ -116,13 +116,16 @@ public class PATGUI extends JFrame implements ActionListener {
 		break;
 		
 		case "Next Question":
-			//can do the same for others to avoid typo problems
-			displayEntireFile();
-		break;
-
-		case "Write to file":
-			tfHandler.appendToFile("myFiles/personality.txt", jTextArea.getText());
-	
+			tfHandler.appendToFile("PAT.myFiles/personality.txt", jTextArea.getText());
+			question.getSessionNumber();
+			question.Question();
+			jbCreateFile.setVisible(false);
+			jTextArea.setVisible(true);
+			jlSession.setVisible(true);
+			jlSpace.setVisible(true);
+			jlAnswer.setVisible(true);
+			jbNextQuestion.setVisible(true);
+			break;
 	}
 		
 	}
@@ -140,7 +143,7 @@ public class PATGUI extends JFrame implements ActionListener {
 		String fileName = "myFiles/assessment.txt";
 				//fileName = jTextField.getText();//get from the textfield where the user types the filename
 		tfHandler.createNewFile(fileName); //use your tfHandler
-		jTextArea.append("Done creating file"+fileName);
+		//jTextArea.append("Done creating file"+fileName);
 		//update the display to notify user that the file was created
 	
 	
